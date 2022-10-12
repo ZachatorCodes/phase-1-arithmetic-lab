fc79efb2d108efc5d7b2c309cf3af4055b194688
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-12 15:36:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// For Multiply Test
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// For Random Test
+const random = Math.floor(Math.random() * 10) + 1;
+
+// For Mod Test
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+
+// For Max Test
+const max = Math.max(4, 8, 20, 5, 7);
